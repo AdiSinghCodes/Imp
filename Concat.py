553aed7e976336eab7e7cdd05e71e378bff1a5ed
AdiSinghCodes <adityasinghgithub@gmail.com>
2026-08-14 23:40:37 +0530

    Python-Practice-1

---
+++ Concat.py
@@ -0,0 +1,16 @@
+size = int(input("Enter the size of list"))
+list = []
+for i in range(size):
+    list.append(int(input(i)))
+
+print(list)
+
+result = []
+
+for num in list:
+    result.append(num)
+
+for num in list:
+    result.append(num)
+
+print(result)
