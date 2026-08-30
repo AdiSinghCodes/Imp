46d99a67ee465830b7088a1ec80e48a91d184259
AdiSinghCodes <adityasinghgithub@gmail.com>
2026-08-30 12:18:17 +0530

    add-array

---
+++ Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] concatWithReverse(int[] nums) {
+        int[] ans = new int[2*nums.length];
+        for(int i=0; i<nums.length; i++)
+        {
+            ans[i] = nums[i];
+        }
+        int m = nums.length-1;
+        for(int i=nums.length; i<ans.length; i++)
+        {
+            ans[i] = nums[m--];
+        }
+        return ans;
+        
+    }
+}
